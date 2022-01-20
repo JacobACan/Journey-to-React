dfdc03aa1b2ad8e13174462aced8d7cbfe7f36b3
Jacob Canedy <jcanedy19@gmail.com>
2022-01-20 10:20:31 -0500

    Switch statements and Ternary Operators

---
+++ JS_Learning/Control_Flow/script.js
@@ -0,0 +1,49 @@
+// Ternary Operator
+// check ? run() : runElse
+// isLoggedin = false
+// isLoggedin ? console.log("Is Logged In") : console.log("Is Not Logged In")
+
+// //Switch statement
+// /* If you want to comare one variable and check if it is directly equal to another value multiple times*/
+// const favoriteAnimal = "shark"
+// /*switch(variable you want to check) {scope
+//     case
+// }*/
+// switch (favoriteAnimal) {
+//   case "cheetah":
+//   case "bobcat":
+//   case "cat": // if equal to cat then
+//     console.log("cats are pretty cool")
+//     break
+//   case "dog":
+//     console.log("wow they are loud")
+//     break
+//   case "shark":
+//     console.log("that's an interesting choice")
+//     break
+//   default:
+//     // Else
+//     console.log("What kinda animal is that?!?!")
+// }
+
+const number = 0
+
+switch (number) {
+  case 0:
+    console.log("The Number is 0")
+    break
+  case 1:
+  case 2:
+    console.log("The Number is small")
+    break
+  case 3:
+  case 4:
+    console.log("The Number is medium")
+    break
+  case 5:
+    console.log("The Number is large")
+    break
+  default:
+    console.log("try again!")
+    break
+}
