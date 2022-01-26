d3dc1526b9e98ecaf0939c41417a898a5f83abe0
Jacob Canedy <jcanedy19@gmail.com>
2022-01-26 09:42:30 -0500

    Understanding Instructor's Solution

--- JS_Learning/53-midi-piano/before/script.js
+++ JS_Learning/53-midi-piano/before/script.js
@@ -12,74 +12,97 @@ const NOTE_DETAILS = [
   { note: "Bb", key: "J", frequency: 466.164 },
   { note: "B", key: "M", frequency: 493.883 },
 ]
-const KEYS = document.querySelectorAll(".key")
-var audioCtx = new (window.AudioContext || window.webkitAudioContext)()
+// // My Solution
+// const KEYS = document.querySelectorAll(".key")
+// var audioCtx = new (window.AudioContext || window.webkitAudioContext)()
 
-function pressKey(key) {
-  playNote(key)
-  key.classList.add("active")
-}
-function unpressKey(key) {
-  stopNote(key)
-  key.classList.remove("active")
-}
+// function pressKey(key) {
+//   playNote(key)
+//   key.classList.add("active")
+// }
+// function unpressKey(key) {
+//   stopNote(key)
+//   key.classList.remove("active")
+// }
 
-function playNote(key) {
-  NOTE_DETAILS.forEach((note) => {
-    if (note.note == key.dataset.note) {
-      oscillator = audioCtx.createOscillator()
-      createNote(note.frequency, oscillator)
-    }
-  })
-}
-function stopNote(key) {
-  NOTE_DETAILS.forEach((note) => {
-    if (note.note == key.dataset.note) {
-      oscillator.stop()
-    }
-  })
-}
+// function playNote(key) {
+//   NOTE_DETAILS.forEach((note) => {
+//     if (note.note == key.dataset.note) {
+//       oscillator = audioCtx.createOscillator()
+//       createNote(note.frequency, oscillator)
+//     }
+//   })
+// }
+// function stopNote(key) {
+//   NOTE_DETAILS.forEach((note) => {
+//     if (note.note == key.dataset.note) {
+//       oscillator.stop()
+//     }
+//   })
+// }
 
-function createNote(frequency, oscillator) {
-  oscillator.type = "saw"
-  oscillator.frequency.value = frequency
-  oscillator.connect(audioCtx.destination)
-  oscillator.start()
-}
+// function createNote(frequency, oscillator) {
+//   oscillator.type = "saw"
+//   oscillator.frequency.value = frequency
+//   oscillator.connect(audioCtx.destination)
+//   oscillator.start()
+// }
 
-KEYS.forEach((key) => {
-  key.addEventListener("mouseup", (e) => {
-    keysClasses = key.classList
-    console.log(e)
-    unpressKey(key)
-  })
-})
-KEYS.forEach((key) => {
-  key.addEventListener("mousedown", (e) => {
-    console.log(e)
-    pressKey(key)
-  })
-})
-document.addEventListener("keyup", (e) => {
-  NOTE_DETAILS.forEach((key) => {
-    if (key.key.toLowerCase() == e.key) {
-      KEYS.forEach((htmlKey) => {
-        if (key.note == htmlKey.dataset.note) {
-          unpressKey(htmlKey)
-        }
-      })
-    }
-  })
-})
+// KEYS.forEach((key) => {
+//   key.addEventListener("mouseup", (e) => {
+//     keysClasses = key.classList
+//     console.log(e)
+//     unpressKey(key)
+//   })
+// })
+// KEYS.forEach((key) => {
+//   key.addEventListener("mousedown", (e) => {
+//     console.log(e)
+//     pressKey(key)
+//   })
+// })
+// document.addEventListener("keyup", (e) => {
+//   NOTE_DETAILS.forEach((key) => {
+//     if (key.key.toLowerCase() == e.key) {
+//       KEYS.forEach((htmlKey) => {
+//         if (key.note == htmlKey.dataset.note) {
+//           unpressKey(htmlKey)
+//         }
+//       })
+//     }
+//   })
+// })
+// document.addEventListener("keydown", (e) => {
+//   if (e.repeat) return
+//   NOTE_DETAILS.forEach((key) => {
+//     if (key.key.toLowerCase() == e.key) {
+//       KEYS.forEach((htmlKey) => {
+//         if (key.note == htmlKey.dataset.note) {
+//           pressKey(htmlKey)
+//         }
+//       })
+//     }
+//   })
+// })
+
+// Boss Man's Solution
 document.addEventListener("keydown", (e) => {
-  if (e.repeat) return
-  NOTE_DETAILS.forEach((key) => {
-    if (key.key.toLowerCase() == e.key) {
-      KEYS.forEach((htmlKey) => {
-        if (key.note == htmlKey.dataset.note) {
-          pressKey(htmlKey)
-        }
-      })
-    }
-  })
+  if (e.repeat) return // if key is being held then don't continue
+  let noteDetailsKey = getNoteDetails(e.key) // Get the object key in js based on key press
+  console.log(noteDetailsKey)
+
+  if (noteDetailsKey == null) return
+  noteDetailsKey.active = true
+  playNotes()
 })
+
+function getNoteDetails(keyboardKey) {
+  return NOTE_DETAILS.find(
+    (noteDetailsKey) => keyboardKey === noteDetailsKey.key.toLowerCase()
+  )
+}
+
+function playNotes() {
+  let htmlKey = document.querySelectorAll(".active")
+  console.log(htmlKey)
+}
