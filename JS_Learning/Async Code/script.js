62aff800bbe25e16fb94d3a28003ba942ef0c853
Jacob Canedy <jcanedy19@gmail.com>
2022-02-09 09:38:14 -0500

    Async Await

--- JS_Learning/Async Code/script.js
+++ JS_Learning/Async Code/script.js
@@ -28,55 +28,96 @@
 //     console.log("here")
 // }, 500)
 
-function setTimeoutPromise(duration) {
+// function setTimeoutPromise(duration) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(resolve("Hello"), duration)
+//   })
+// }
+
+// setTimeoutPromise(500)
+//   .then(() => {
+//     console.log("here1")
+//     return setTimeoutPromise(1000)
+//   })
+//   .then(() => {
+//     console.log("here2")
+//     return setTimeoutPromise(1000)
+//   })
+//   .then(() => {
+//     console.log("here3")
+//     return setTimeoutPromise(1000)
+//   })
+
+// /*
+// //Create a promise version of addEventListener
+// const button = document.querySelector("button")
+// function addEventListenerPromise(element, method) {
+//   return new Promise((resolve, reject) => {
+//     element.addEventListener(method, (e) => {
+//       resolve(e)
+//     })
+//   })
+// }
+
+// addEventListenerPromise(button, "click").then((e) => {
+//   console.log("clicked")
+//   console.log("e")
+// })
+// */
+
+// Promise.all([
+//   // First one to fail is in catch
+//   setTimeoutPromise(500),
+//   Promise.all([
+//     setTimeoutPromise(1000),
+//     Promise.resolve("2"),
+//     Promise.resolve("3"),
+//   ]),
+//   Promise.resolve("3"),
+// ])
+//   .then((messages) => {
+//     console.log(messages)
+//   })
+//   .catch((error) => {
+//     console.error(error)
+//   })
+// Promise.any // First one to pass is .thened
+
+// Promise.race // First one to finish
+
+// Promise.allSettled.finally // Waits for everything to resolve then shows everything .catch doesnt come into play // runs no matter what.
+
+//ASYNC
+//  await
+function getValueWithDelay(value, delay) {
   return new Promise((resolve, reject) => {
-    setTimeout(resolve("Hello"), duration)
+    setTimeout(() => {
+      resolve(value)
+    }, delay)
+  })
+}
+function getValueWithDelayError(value, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject(value)
+    }, delay)
   })
 }
 
-setTimeoutPromise(500)
-  .then(() => {
-    console.log("here1")
-    return setTimeoutPromise(1000)
-  })
-  .then(() => {
-    console.log("here2")
-    return setTimeoutPromise(1000)
-  })
-  .then(() => {
-    console.log("here3")
-    return setTimeoutPromise(1000)
-  })
-
-/*
-//Create a promise version of addEventListener
-const button = document.querySelector("button")
-function addEventListenerPromise(element, method) {
-  return new Promise((resolve, reject) => {
-    element.addEventListener(method, (e) => {
-      resolve(e)
-    })
-  })
+async function getValues() {
+  try {
+    let message = await getValueWithDelay(3, 500)
+    console.log(message)
+    message = await getValueWithDelay(3, 500)
+    console.log(message)
+    message = await getValueWithDelayError(4, 500)
+    console.log(message)
+  } catch (e) {
+    // the error is what the value was suppose to be
+    console.log(e)
+  } finally {
+    console.log("Hello")
+  }
 }
 
-addEventListenerPromise(button, "click").then((e) => {
-  console.log("clicked")
-  console.log("e")
-})
-*/
-
-Promise.all([
-  setTimeoutPromise(500),
-  Promise.all([
-    setTimeoutPromise(1000),
-    Promise.resolve("2"),
-    Promise.resolve("3"),
-  ]),
-  Promise.resolve("3"),
-])
-  .then((messages) => {
-    console.log(messages)
-  })
-  .catch((error) => {
-    console.error(error)
-  })
+getValues()
