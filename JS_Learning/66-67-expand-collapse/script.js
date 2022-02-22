4b114fed91fa91fb1fb24510b7f1930098aaa496
Jacob Canedy <jcanedy19@gmail.com>
2022-02-22 10:41:24 -0500

    Expand Collapse Application

--- JS_Learning/66-67-expand-collapse/script.js
+++ JS_Learning/66-67-expand-collapse/script.js
@@ -7,6 +7,7 @@ function toggleExpand(target) {
   Card = target.closest(".card")
   cardBody = Card.querySelector(".card-body")
   cardBody.classList.toggle("show")
+
   if (target.innerText == "Expand") {
     target.innerText = "Collapse"
   } else if (target.innerText == "Collapse") {
