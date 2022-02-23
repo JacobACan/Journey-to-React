1c7d8e3c1caac40a8beef6ba556792d598b3f256
Jacob Canedy <jcanedy19@gmail.com>
2022-02-23 09:45:35 -0500

    NPM UUID

--- JS_Learning/Modules_Bundlers/script.js
+++ JS_Learning/Modules_Bundlers/script.js
@@ -1,4 +1,14 @@
+//ES6 modules
 import { Bear, Joey } from "./user.js"
 
 console.log(Bear)
 console.log(Joey)
+
+// //Node.js Modules
+// const userExport = require("./user.js")
+// console.log(userExport.Julia.fur)
+// console.log(userExport.Dameon.fur)
+
+//NPM
+const uuid = require("uuid")
+console.log(uuid.v4())
--- JS_Learning/Modules_Bundlers/user.js
+++ JS_Learning/Modules_Bundlers/user.js
@@ -8,3 +8,19 @@ export const Joey = {
   width: 2,
   fur: "white",
 }
+export const Julia = {
+  height: 1,
+  width: 2,
+  fur: "pink",
+}
+export const Dameon = {
+  height: 1,
+  width: 2,
+  fur: "Orange",
+}
+
+// //Node.js modules
+// module.exports = {
+//   Julia: Julia,
+//   Dameon: Dameon,
+// }
